80cb1698756b79c899c4c603a52e739ab0f36152
feiye <feiyemenzhu01@163.com>
2025-11-12 15:19:21 +0800

    feat(Thread）:因为不同的原因而阻塞
    + join()：因为另一个线程还没结束而阻塞
    + sleep()：因为设定的时间未到而阻塞。

--- src/main/java/com/feiye/thread/CallerThread.java
+++ src/main/java/com/feiye/thread/CallerThread.java
@@ -1,29 +1,35 @@
-package com.ubug.thread;
+package com.feiye.thread;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class CallerThread {
     public static void main(String[] args) {
         System.out.println("CallerThread");
+        // 这里是非阻塞的
         CompletableFuture.runAsync(() -> {
             System.out.println("thread-1");
         });
+
+        //sleep阻塞：当前线程自己暂停一段时间
         CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(1);
+                TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println("thread-2");
         });
+
+        //join阻塞：线程同步：确保某线程执行完毕后再继续
         CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(1);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println("thread-3");
-        });
+        }).join();
         System.out.println("thread-main");
     }
 }
