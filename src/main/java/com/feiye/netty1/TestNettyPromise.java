6050ffd9bd6cc776da159d81417ba70a4ebfcb4e
feiye-sunjingru <1050963440@qq.com>
2025-11-21 14:46:41 +0800

    feat(netty future+promise）: netty promise由自己主动创建；同步或异步都可以
    + 可以设置成功结果（自己装内容）；失败了可以通知到外部
    + get阻塞获取
    + addListener:nio处理获得结果

---
+++ src/main/java/com/feiye/netty1/TestNettyPromise.java
@@ -0,0 +1,42 @@
+package com.feiye.netty1;
+
+import io.netty.channel.EventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.concurrent.DefaultPromise;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TestNettyPromise {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 1、准备 EventLoop 对象
+        EventLoop eventLoop = new NioEventLoopGroup().next();
+        // 2、可以主动创建 Promise：结果容器
+        DefaultPromise<Integer> promise = new DefaultPromise<>(eventLoop);
+
+        new Thread(() -> {
+            // 3、任意一个线程执行计算，计算完成之后向 promise 填充结果
+            log.debug("开始计算...");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                // 计算出现异常
+                int i = 1 / 0;
+                //自己装结果
+                promise.setSuccess(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                //线程出错会通知到main线程
+                promise.setFailure(e);
+            }
+        }).start();
+
+        // 4、接收结果的线程
+        log.debug("等待结果...");
+        log.debug("结果是：{}", promise.get());
+        // 异步
+//         promise.addListener(future -> log.debug("结果是：{}", future.get()));
+    }
+}
+
