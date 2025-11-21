63af481834d768fe45b424f34cbde27b6b7ae748
feiye-sunjingru <1050963440@qq.com>
2025-11-21 09:32:27 +0800

    feat(netty eventloop）: EventLoopGroup细分为boss和worker
    + boss负责accept，worker负责读写

--- src/main/java/com/feiye/netty1/EventLoopServer.java
+++ src/main/java/com/feiye/netty1/EventLoopServer.java
@@ -10,14 +10,15 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
         new ServerBootstrap()
-                .group(new NioEventLoopGroup())
+                // 参数1： boss（只负责 ServerSocketChannel 上 accept 事件） :只会占用 参数1（boss）里面的一个线程
+                // 参数2： worker（只负责 SocketChannel 上的读写事件）: worker 初始化两个线程（只有两个worker）
+                .group(new NioEventLoopGroup(1), new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
