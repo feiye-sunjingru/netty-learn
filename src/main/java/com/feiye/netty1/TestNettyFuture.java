99b69e178dc09a9c21a35abbb3c102222e34a6f3
feiye-sunjingru <1050963440@qq.com>
2025-11-21 14:31:35 +0800

    feat(netty future+promise）: netty future同步或异步都可以
    + get阻塞获取
    + addListener:nio处理获得结果

---
+++ src/main/java/com/feiye/netty1/TestNettyFuture.java
@@ -0,0 +1,40 @@
+package com.feiye.netty1;
+
+import io.netty.channel.EventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TestNettyFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //netty的线程：EventLoop是一个现成
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        EventLoop eventLoop = group.next();
+
+        Future<Integer> future = eventLoop.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                log.debug("执行计算...");
+                TimeUnit.SECONDS.sleep(3);
+                return 50;
+            }
+        });
+        log.debug("等待结果...");
+        // 同步方式[main]
+        //log.debug("结果是：{}", future.get());
+        // 异步方式
+        future.addListener(new GenericFutureListener<Future<? super Integer>>() {
+            @Override
+            public void operationComplete(Future<? super Integer> future1) throws Exception {
+                //此时用get也可以，因为线程到这里已经拿到结果了[nioEventLoopGroup-2-1]
+                log.debug("结果是：{}", future1.getNow());
+            }
+        });
+    }
+}
