0ae8e06e3500756e3713302783bd895e86b15a31
feiye-sunjingru <1050963440@qq.com>
2025-11-21 15:56:01 +0800

    feat(netty pipeline）: EmbeddedChannel模拟入站和出站操作

---
+++ src/main/java/com/feiye/netty1/pipeline/TestEmbeddedChannel.java
@@ -0,0 +1,65 @@
+package com.feiye.netty1.pipeline;
+
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.embedded.EmbeddedChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestEmbeddedChannel {
+    public static void main(String[] args) {
+        // 入站处理器
+        ChannelInboundHandlerAdapter h1 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("h1");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelInboundHandlerAdapter h2 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("h2");
+                super.channelRead(ctx, msg);
+            }
+        };
+        // 出站处理器
+        ChannelInboundHandlerAdapter h3 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.debug("h3");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelOutboundHandlerAdapter h4 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("h4");
+                super.write(ctx, msg, promise);
+            }
+        };
+        ChannelOutboundHandlerAdapter h5 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("h5");
+                super.write(ctx, msg, promise);
+            }
+        };
+        ChannelOutboundHandlerAdapter h6 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.debug("h6");
+                super.write(ctx, msg, promise);
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(h1, h2, h3, h4, h5, h6);
+        // 模拟入站
+//        channel.writeInbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("hello".getBytes()));
+        // 模拟出站
+        channel.writeOutbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("hello".getBytes()));
+    }
+}
