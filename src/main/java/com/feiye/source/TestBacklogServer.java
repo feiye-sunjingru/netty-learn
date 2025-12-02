32fde983f23051341ff9c10de336f1976c5bc523
feiye-sunjingru <1050963440@qq.com>
2025-12-02 15:49:20 +0800

    feat(netty 优化）: 服务端ssc设置全连接个数，正常应该设置大些.为了测试全连接一直没释放的状态，可以在服务端调试（对应方法加断点）
    + 4.1.39.Final netty-all 可以通过NioEventloop#processSelectedKey方法上加断点
    + 4.2.7 netty-all 可以通过AbstractChannelHandlerContext#bind方法上加断点
    + 当第三个连接到时会收到服务器拒绝连接的错误

---
+++ src/main/java/com/feiye/source/TestBacklogServer.java
@@ -0,0 +1,31 @@
+package com.feiye.source;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * 断点放在：netty-all 4.1.39.Final NioEventLoop if ((readyOps & (SelectionKey.OP_READ | SelectionKey.OP_ACCEPT)) != 0 || readyOps == 0) {
+ * 当出现超过2个客户端连接时，客户端那边会显示“ Connection refused: getsockopt”
+ */
+public class TestBacklogServer {
+    public static void main(String[] args) {
+        // 本地处理accept 连接非常快：处理的快不会在队列里堆积：所以为了测试这边加了断点保持全连接队列
+        ChannelFuture channelFuture = new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                //全连接队列设置: 客户端连接数一旦超过，客户端显示“服务器会拒绝新的连接”
+                .option(ChannelOption.SO_BACKLOG, 2)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                }).bind(8080);
+    }
+}
